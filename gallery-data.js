ba4a24d9b0df275eafe0b4d4ff0f6fac8a1d9e4d
PiramGFX <piramspotify4@gmail.com>
2026-09-05 20:30:05 +0900

    Upload Piram GFX website

---
+++ gallery-data.js
@@ -0,0 +1,24 @@
+/*
+  작품을 바꾸거나 추가하는 파일입니다.
+
+  1. images 폴더에 사진을 넣습니다.
+  2. 아래 src를 실제 파일명으로 바꿉니다.
+  3. alt에는 사진을 짧게 설명하는 문장을 적습니다.
+  4. title을 빈칸으로 두면 확대 화면에도 제목이 표시되지 않습니다.
+
+  항목 한 개의 모양:
+  { src: "images/파일명.webp", alt: "작품 설명", title: "선택 제목" },
+*/
+
+window.PIRAM_GALLERY = [
+  {
+    src: "images/GFX-1.jpg",
+    alt: "Moonlight",
+    title: "Moonlight",
+  },
+  {
+    src: "images/Creating.png",
+    alt: "creating",
+    title: "",
+  }
+];
